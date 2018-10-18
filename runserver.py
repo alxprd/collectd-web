bf4f58924abc576aedd48dc1295801fb9774e8a1
Alejandro Pereda <atk1005@gmail.com>
2018-10-19 07:16:37 +0800

    Allows access to any ip.

--- runserver.py
+++ runserver.py
@@ -16,8 +16,8 @@ def main():
         httpd = BaseHTTPServer.HTTPServer((args[0], int(args[1])), Handler)
         print "Collectd-web server running at http://%s:%s/" % (args[0], args[1])
     else:
-        httpd = BaseHTTPServer.HTTPServer(("127.0.0.1", PORT), Handler)
-        print "Collectd-web server running at http://%s:%s/" % ("127.0.0.1", PORT)
+        httpd = BaseHTTPServer.HTTPServer(("0.0.0.0", PORT), Handler)
+        print "Collectd-web server running at http://%s:%s/" % ("0.0.0.0", PORT)
     httpd.serve_forever()
 
 if __name__ == "__main__":
